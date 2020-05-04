b8cc39d7aab111b01b4cef5cb85b976a4e425770
MiroIGPM <43861586+MiroIGPM@users.noreply.github.com>
2020-05-04 16:49:05 +0200

    Update Search.js

--- src/Commponents/Container/Search/Search.js
+++ src/Commponents/Container/Search/Search.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from "react";
+import React, {  useEffect } from "react";
 
 //component import
 import Auxiliary from "../../../hoc/Auxiliary/Auxiliary";
@@ -14,7 +14,7 @@ import {
     resetTopNews,
 } from "../../../actions/newsActions";
 
-<<<<<<< HEAD
+
 const Search = (props) => {
     const {
         activeCountry,
@@ -50,54 +50,7 @@ const Search = (props) => {
                 urlToImage={urlToImage}
                 description={description}
             />
-=======
-class Search extends Component {
-    componentDidMount() {
-        this.props.resetNews();
-    }
 
-    componentDidUpdate(prevProps) {
-        if (this.props.activeCountry !== prevProps.activeCountry) {
-            this.props.search();
-        }
-    }
-
-    render() {
-        let searchedNews = this.props.topNews.map((article) => {
-            const { id, title, urlToImage, description } = article;
-            return (
-                <Thumbnail
-                    key={id}
-                    id={id}
-                    title={title}
-                    urlToImage={urlToImage}
-                    description={description}
-                />
-            );
-        });
-
-        return (
-            <Auxiliary>
-                <h1
-                    className={classes["Title"]}
-                >{`Search top news from ${this.props.countryName} by term:`}</h1>
-                <div className={classes["InputHolder"]}>
-                    <input
-                        type="text"
-                        placeholder="Search tearm..."
-                        className={classes["Input"]}
-                        onChange={this.props.handleInput}
-                        onBlur={this.props.search}
-                    ></input>
-                </div>
-                <div>
-                    <GridHolder loading={this.props.loading}>
-                        {searchedNews}
-                    </GridHolder>
-                </div>
-            </Auxiliary>
->>>>>>> 7f8afe59c9822338cf3509dd2517dae1d2fd5537
-        );
     });
 
     return (
